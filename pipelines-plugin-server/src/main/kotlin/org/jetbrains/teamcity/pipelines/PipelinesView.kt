3357ca3f385fba14980da96442f562d8f27bcd5a
Nikita Skvortsov <nikita.skvortsov@jetbrains.com>
2014-04-09 16:29:44 +0400

    optimize imports

--- pipelines-plugin-server/src/main/kotlin/org/jetbrains/teamcity/pipelines/PipelinesView.kt
+++ pipelines-plugin-server/src/main/kotlin/org/jetbrains/teamcity/pipelines/PipelinesView.kt
@@ -7,16 +7,6 @@ import jetbrains.buildServer.web.openapi.PluginDescriptor
 import javax.servlet.http.HttpServletRequest
 import jetbrains.buildServer.serverSide.SProject
 import jetbrains.buildServer.users.SUser
-import jetbrains.buildServer.controllers.admin.projects.EditProjectTab
-import jetbrains.buildServer.serverSide.SBuildServer
-import jetbrains.buildServer.web.openapi.WebControllerManager
-import javax.servlet.http.HttpServletResponse
-import org.springframework.web.servlet.ModelAndView
-import jetbrains.buildServer.controllers.BaseFormXmlController
-import org.jdom.Element
-import jetbrains.buildServer.controllers.FormUtil
-import jetbrains.buildServer.web.openapi.ControllerAction
-import java.util.ArrayList
 
 /**
  * Created by Nikita.Skvortsov
